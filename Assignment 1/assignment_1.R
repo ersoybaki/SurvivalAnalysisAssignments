25c048fc02e888fac427d11e70a755e673ad45aa
jojomaverik <arda.bizeldodo@gmail.com>
2026-09-15 19:15:41 +0200

    Fixxed set and variable issue

--- Assignment 1/assignment_1.R
+++ Assignment 1/assignment_1.R
@@ -8,13 +8,15 @@ target_pc <- 0.1 # CHANGE TO 0, 0.1, 0.5
 R <- 2000
 
 # Estimators
-naive_est <- numeric(1)
+# naive_est <- numeric(1)
+
+T_cal <- rexp(n, rate = true_rate)
 
 # Function to find c_max
 # ! This function was generated with Gemini 3.6 Flash (Ersoy). I will add it to the report ! 
 obj_fn <- function(c_max) {
   C_sim <- runif(n, 0, c_max)
-  empirical_pc <- mean(C_sim < T)
+  empirical_pc <- mean(C_sim < T_cal)
   return(empirical_pc - target_pc)
 }
 C_sim_fit <- uniroot(obj_fn, interval = c(1, 2000))
@@ -25,16 +27,16 @@ mean_Y <- numeric(R)
 
 for (r in 1:R) {
   # Times 
-  T <- rexp(n, rate=true_rate)
+  T_i <- rexp(n, rate=true_rate)
   
   # Right Censoring
   C <- runif(n, 0, C_sim_fit$root)
   
   # Observed data
-  Y <- pmin(T, C)
+  Y <- pmin(T_i, C)
   
   # Mean Y calculation for Naive est
-  mean_Y <- sum(Y) / n
+  mean_Y[r] <- sum(Y) / n
   
 }
 
